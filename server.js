161a69e6ae7172956bc27b83181e4680d213ea31
d8musica <d8musica@gmail.com>
2019-07-19 10:23:22 -0500

    mongoose workin'

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const next = require('next')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const url = 'mongodb+srv://admin:admin@cluster0-9doht.mongodb.net/test?retryWrites=true&w=majority'
+
+var Schema = mongoose.Schema;
+
+  var userSchema = new Schema({
+    email:  String,
+    password: String,
+    username:   String,    
+  });
+
+var User = mongoose.model('User', userSchema);
+
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+  if (err) return reject(err)
+  console.log('Chupelo!')
+  })  
 
 app.prepare().then(() => {
   const server = express()
@@ -14,7 +31,13 @@ app.prepare().then(() => {
 
   server.post('/a', (req, res) => {
     console.log(req.body)
-    return res.send("soy mk")
+
+    const newUser = new User(req.body)
+
+    newUser.save()
+    
+    console.log(newUser)
+
   })
 
   server.get('*', (req, res) => {
